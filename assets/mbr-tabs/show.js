9624ce1bad6b2a2a412055486dae8a3a6d99befd
minegif <davidfavour40027@gmail.com>
2026-05-06 18:08:42 +0300

    Update show.js

--- assets/mbr-tabs/show.js
+++ assets/mbr-tabs/show.js
@@ -13,16 +13,16 @@ function showRefine() { var abtElement = document.getElementById("refine");
 
 var button = document.getElementById('refiner');
 
-if (abtElement.style.display === "none") { abtElement.style.display = "block"; button.textContent = 'Show Less'; } else { abtElement.style.display = "none"; button.textContent = 'Learn More';} }
+if (abtElement.style.display === "none") { abtElement.style.display = "block"; abtElement.classList.add('scroll-box'); button.textContent = 'Show Less'; } else { abtElement.style.display = "none"; button.textContent = 'Learn More';} }
 
 function showTrade() { var abtElement = document.getElementById("trade"); 
 
 var button = document.getElementById('trader');
 
-if (abtElement.style.display === "none") { abtElement.style.display = "block"; button.textContent = 'Show Less';} else { abtElement.style.display = "none"; button.textContent = 'Learn More';} }
+if (abtElement.style.display === "none") { abtElement.style.display = "block"; abtElement.classList.add('scroll-box'); button.textContent = 'Show Less';} else { abtElement.style.display = "none"; button.textContent = 'Learn More';} }
 
 function showInvest() { var abtElement = document.getElementById("invest"); 
 
 var button = document.getElementById('invester');
 
-if (abtElement.style.display === "none") { abtElement.style.display = "block"; button.textContent = 'Show Less';} else { abtElement.style.display = "none"; button.textContent = 'Learn More';} }
+if (abtElement.style.display === "none") { abtElement.style.display = "block"; abtElement.classList.add('scroll-box'); button.textContent = 'Show Less';} else { abtElement.style.display = "none"; button.textContent = 'Learn More';} }
